4eee61fe24ffe3ba354fe5ca67707b6b34f8a1df
niamuladze <niamuladze08@gmail.com>
2025-02-26 19:26:47 +0400

    commit

---
+++ day 19/classwork/classwork.py
@@ -0,0 +1,11 @@
+#1st work
+for i in range(150, 202, 2):
+    print(i)
+
+#2nd work
+for i in range(200, 400):
+    print(i)
+
+#3rd work
+for goa in range(50):
+    print("GOA BEST!!!")
---
+++ day 19/homework/homework.py
@@ -0,0 +1,15 @@
+#1st work
+for goa in range(100):
+    print("GOA BEST!!!")
+
+
+#2nd work
+i = 1
+while i < 50:
+    print(i)
+    i += 1
+
+
+#3rd work
+for name in range(7):
+    print("i love programming")
