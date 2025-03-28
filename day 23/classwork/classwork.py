bfe2a0f14779147fb85734f1bfc652f391cbc478
niamuladze <niamuladze08@gmail.com>
2025-03-28 21:48:35 +0400

    commit

---
+++ day 23/classwork/classwork.py
@@ -0,0 +1,19 @@
+#1
+numbers = [10, 20, 30, 40, 50]
+print("პირველი ელემენტი:", numbers[0])
+print("ბოლო ელემენტი:", numbers[-1])
+print("შუა ელემენტი:", numbers[len(numbers) // 2])
+
+#2
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12]  # შესწორებულია, გამეორებული რიცხვები (3 და 6) შეიცვალა 2-ით და 7-ით
+print(num)
+
+#3
+fruits = ["banana", "orange", "grape"]
+fruits.append("apple")
+print(fruits)
+
+#4
+animals = ["cat", "dog", "elephant", "tiger"]
+index = animals.index("dog")
+print("Dog-ის ინდექსი:", index)
